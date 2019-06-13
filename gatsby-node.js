51ad8f6cba7cde6c89e4fd0dc9b74cce55b3af88
Konrad Beśka <beska.konrad@gmail.com>
2019-06-13 20:20:40 +0200

    Szablony: ujednolicenie nazewnictwa

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,7 +17,7 @@ module.exports.onCreateNode = ({ node, actions }) => {
 
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions;
-    const blogTemplate = path.resolve('./src/templates/blog.js');
+    const blogTemplate = path.resolve('./src/templates/blog-post.js');
     const blogIndexTemplate = path.resolve('./src/templates/blog-index.js');
     const res = await graphql(`
         query {
